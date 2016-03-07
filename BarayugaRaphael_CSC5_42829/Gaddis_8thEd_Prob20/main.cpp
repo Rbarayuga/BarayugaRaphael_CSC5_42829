6036ec035a82c7b9c6f81fa53f69cb4bbd28c931
Rbarayuga <Raphael.barayuga@gmail.com>
2016-03-07 10:56:38 -0800

    Added files via upload

---
+++ BarayugaRaphael_CSC5_42829/Gaddis_8thEd_Prob20/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Raphael M.B. Barayuga
+ *
+ * Created on March 7, 2016, 10:48 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+//Global constant
+ */
+const PI = atan(1.0)* 4
+int main(int argc, char** argv) {
+// Declare Variables 
+    int diapiza, slice = 14.125 
+    
+    return 0;
+}
+
